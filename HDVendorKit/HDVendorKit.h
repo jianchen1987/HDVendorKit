f8747579b0dd34e3546cbbac36a7e4955a8e04b4
chenj <chenj@kh-super.com>
2023-04-06 18:37:35 +0800

    published HDVendorKit 0.4.4

--- HDVendorKit/HDVendorKit.h
+++ HDVendorKit/HDVendorKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDVendorKit_VERSION = @"0.4.3";
+static NSString *const HDVendorKit_VERSION = @"0.4.4";
 
 #if __has_include("HDWebImageManager.h")
 #import "HDWebImageManager.h"
--- HDVendorKit/HDWebImageManager/HDWebImageManager.m
+++ HDVendorKit/HDWebImageManager/HDWebImageManager.m
@@ -8,6 +8,9 @@
 
 #import "HDWebImageManager.h"
 
+static SDWebImageDownloader *_ImageDownloader = nil;
+static dispatch_once_t onceToken;
+
 @implementation HDWebImageManager
 + (void)setImageWithURL:(nullable NSString *)url placeholderImage:(nullable UIImage *)placeholder imageView:(nullable UIImageView *)imageView {
     [self setImageWithURL:url size:CGSizeZero placeholderImage:placeholder imageView:imageView completed:nil];
@@ -41,11 +44,22 @@
         }
         return;
     }
+    
+    
+    dispatch_once(&onceToken,^{
+        SDWebImageDownloaderConfig *config = [[SDWebImageDownloaderConfig alloc] init];
+        config.maxConcurrentDownloads = 10;
+        config.downloadTimeout = 30;
+        _ImageDownloader = [[SDWebImageDownloader alloc] initWithConfig:config];
+    });
+    
     [imageView sd_setImageWithURL:[NSURL URLWithString:url]
                  placeholderImage:placeholder
-                          options:0
+                          options:SDWebImageRetryFailed | SDWebImageHighPriority | SDWebImageProgressiveLoad
+                          context:@{SDWebImageContextImageLoader : _ImageDownloader}
                          progress:progressBlock
                         completed:^(UIImage *_Nullable image, NSError *_Nullable error, SDImageCacheType cacheType, NSURL *_Nullable imageURL) {
+        
                             if (error || !image) {
                                 if (size.width > 0 && size.height > 0) {
                                     imageView.image = [placeholder hd_imageResizedWithScreenScaleInLimitedSize:size];
@@ -55,7 +69,9 @@
                                 if (completedBlock) {
                                     completedBlock(image, error, cacheType, imageURL);
                                 }
+                                
                             } else {
+                                
                                 if (completedBlock) {
                                     if (size.width > 0 && size.height > 0) {
                                         completedBlock([image hd_imageResizedWithScreenScaleInLimitedSize:size], error, cacheType, imageURL);
@@ -98,9 +114,19 @@
         imageView.image = placeholder;
         return;
     }
+    
+    dispatch_once(&onceToken,^{
+        SDWebImageDownloaderConfig *config = [[SDWebImageDownloaderConfig alloc] init];
+        config.maxConcurrentDownloads = 10;
+        config.downloadTimeout = 30;
+        _ImageDownloader = [[SDWebImageDownloader alloc] initWithConfig:config];
+    });
+    
+    
     [imageView sd_setImageWithURL:[NSURL URLWithString:url]
                  placeholderImage:placeholder
-                          options:0
+                          options:SDWebImageRetryFailed | SDWebImageHighPriority
+                          context:@{SDWebImageContextImageLoader : _ImageDownloader}
                          progress:progressBlock
                         completed:completedBlock];
 }
