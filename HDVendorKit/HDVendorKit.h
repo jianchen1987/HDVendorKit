5bee75e516da889f731436949734efdafb010789
肖志勇 <xiaozy@kh-super.com>
2023-04-10 10:40:26 +0800

    升级 sdweb

--- HDVendorKit/HDVendorKit.h
+++ HDVendorKit/HDVendorKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDVendorKit_VERSION = @"0.4.4";
+static NSString *const HDVendorKit_VERSION = @"0.4.5";
 
 #if __has_include("HDWebImageManager.h")
 #import "HDWebImageManager.h"
